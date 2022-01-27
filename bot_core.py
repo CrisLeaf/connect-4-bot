89c1c098037e8a74ffc85cefa15392c6243fc0d8
Cris Torres <cristobal_javier@hotmail.com>
2022-01-27 19:53:39 -0300

    Bot working (not so well)

--- bot_core.py
+++ bot_core.py
@@ -1,58 +1,70 @@
-import pickle
-
-with open("classifier.pkl", "rb") as f:
-	classifier = pickle.load(f)
-	
-#%%
-import pandas as pd
-
-df = pd.read_csv("c4_game_database.csv")
-df = df.sample(frac=1)
-df = df.copy()
-
-#%%
-classifier.predict(df.drop(columns="winner")[0:100])
-
-#%%
 import numpy as np
-
-ROW_COUNT = 6
-COLUMN_COUNT = 7
-zeros_mat = np.zeros((ROW_COUNT, COLUMN_COUNT))
-
-#%%
-new_df = pd.DataFrame(data=zeros_mat.reshape(1, -1), columns=df.columns[0:42])
+import pickle
+from game_core import ROWS_COUNT, COLUMNS_COUNT
 
 
-#%%
-zeros_mat = np.zeros((ROW_COUNT, COLUMN_COUNT))
-
-# for j in [1, 3, 4, 5]:
-# 	zeros_mat[5, j] = 1
-# 	zeros_mat[4, j] = 1
-
-
-print(zeros_mat)
-
-#%%
-prediction_probabilities = []
-for j in range(7):
-	zeros_mat_ = zeros_mat.copy()
-
-	try:
-		available_index = np.where(zeros_mat[:, j] != 0)[0][0] - 1
-		if available_index >= 0:
-			zeros_mat_[available_index, j] = -1
-		else:
-			continue
-	except:
-		zeros_mat_[5, j] = -1
+class GameBot():
 	
-	print(zeros_mat_)
-	prediction = classifier.predict_proba(zeros_mat_.reshape(1, -1))
-	print(prediction)
-	prediction_probabilities.append(prediction[0][0])
-	print("\n")
+	def __init__(self, rows_count=ROWS_COUNT, columns_count=COLUMNS_COUNT):
+		self.rows_count = rows_count
+		self.columns_count = columns_count
+		
+	def load_next_moves_classifier(self):
+		with open("classifier.pkl", "rb") as f:
+			self.classifier = pickle.load(f)
 	
-print(prediction_probabilities)
+		return self
+	
+	def get_next_move(self, played_mat):
+		"""
+		Get the move to play, based on win probabilities of each possible next move.
+		
+		Params
+		------
+		- played_mat, numpy array : the previous played matrix on which the next move
+									corresponds to the bot.
+		
+		Returns
+		-------
+		- next_move, int : the next move column index that the bot should play.
+		"""
+		pred_probas = []
+		
+		# Loop and Predict on available movements
+		for j in range(self.columns_count):
+			played_mat_copy = played_mat.copy()
+			
+			# Fill a piece on the available position in column j, if not continue with the loop
+			try:
+				available_position = np.where(played_mat[:, j] != 0)[0][0] - 1
+			except:
+				available_position = self.rows_count - 1
+			
+			if available_position >= 0:
+				played_mat_copy[available_position, j] = -1
+			else:
+				continue
+			
+			# Predict the win probability
+			pred_proba = self.classifier.predict_proba(played_mat_copy.reshape(1, -1))
+			pred_probas.append(pred_proba[0][0])
+			
+		next_move = pred_probas.index(max(pred_probas))
+		
+		return next_move
 
+
+# played_mat = np.zeros((ROWS_COUNT, COLUMNS_COUNT))
+#
+# for j in [1, 3, 4, 5, 6]:
+# 	played_mat[5, j] = 1
+# 	played_mat[4, j] = 1
+# 	played_mat[3, j] = 1
+#
+#
+# print(played_mat)
+#
+# bot = GameBot()
+# bot.load_next_moves_classifier()
+# next_move = bot.get_next_move(played_mat)
+# print(next_move)
--- game_app.py
+++ game_app.py
@@ -1,162 +1,87 @@
-import numpy as np
 import pygame
 import sys
 import math
+from game_core import (
+	create_board, drop_piece, is_valid_location, get_next_open_row,
+	print_board, winning_move, draw_board,
+	SQUARE_SIZE, RADIUS, width, height, size,
+	BLUE, RED, YELLOW, BLACK
+)
+from bot_core import GameBot
 
-
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-BLACK = (0, 0, 0)
-
-ROW_COUNT = 6
-COLUMN_COUNT = 7
-
-def create_board():
-	board = np.zeros((ROW_COUNT, COLUMN_COUNT))
-	return board
-
-def drop_piece(board, row, col, piece):
-	board[row][col] = piece
-
-def is_valid_location(board, col):
-	return board[ROW_COUNT-1][col] == 0
-
-def get_next_open_row(board, col):
-	for r in range(ROW_COUNT):
-		if board[r][col] == 0:
-			return r
-
-def print_board(board):
-	print(np.flip(board, 0))
-	
-def winning_move(board, piece):
-	# Check horizontal locations for win
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT):
-			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and \
-				board[r][c+3] == piece:
-				return True
-			
-	# Check vertical locations for win
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and \
-				board[r+3][c] == piece:
-				return True
-		
-	# Check positive slope diagonal
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and \
-				board[r+3][c+3] == piece:
-				return True
-	
-	# Check negative slope diagonal
-	for c in range(COLUMN_COUNT-3):
-		for r in range(3, ROW_COUNT):
-			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and \
-				board[r-3][c+3] == piece:
-				return True
-
-def draw_board(board):
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):
-			pygame.draw.rect(screen, BLUE,
-							 (c*SQUARE_SIZE, r*SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-			pygame.draw.circle(
-				screen, BLACK, (int(c*SQUARE_SIZE + SQUARE_SIZE/2),
-								int(r*SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE/2)), RADIUS
-			)
-			
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):
-			if board[r][c] == 1:
-				pygame.draw.circle(
-					screen, RED, (int(c*SQUARE_SIZE + SQUARE_SIZE/2),
-								  height - int(r*SQUARE_SIZE + SQUARE_SIZE/2)), RADIUS
-				)
-			elif board[r][c] == -1:
-				pygame.draw.circle(
-					screen, YELLOW, (int(c * SQUARE_SIZE + SQUARE_SIZE/2),
-									height - int(r*SQUARE_SIZE + SQUARE_SIZE/2)), RADIUS
-				)
-				
-	pygame.display.update()
-				
+# Board Initialization
 board = create_board()
 # print_board(board)
 game_over = False
 turn = 0
 
 pygame.init()
-SQUARE_SIZE = 100
-
-width = COLUMN_COUNT * SQUARE_SIZE
-height = (ROW_COUNT+1) * SQUARE_SIZE
-
-size = (width, height)
-
-RADIUS = int(SQUARE_SIZE/2 - 5)
-
 screen = pygame.display.set_mode(size)
-draw_board(board)
+
+draw_board(screen, board)
 pygame.display.update()
 
-myfont = pygame.font.SysFont("monospace", 75)
+my_font = pygame.font.SysFont("monospace", 75)
 
+# Bot Initialization
+bot = GameBot()
+bot.load_next_moves_classifier()
+
+# Game
 while not game_over:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
-			
+		
 		if event.type == pygame.MOUSEMOTION:
 			pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARE_SIZE))
 			x_pos = event.pos[0]
-			if turn == 0:
-				pygame.draw.circle(screen, RED, (x_pos, int(SQUARE_SIZE/2)), RADIUS)
-			else:
-				pygame.draw.circle(screen, YELLOW, (x_pos, int(SQUARE_SIZE/2)), RADIUS)
-		pygame.display.update()
 			
+			pygame.draw.circle(screen, RED, (x_pos, int(SQUARE_SIZE / 2)), RADIUS)
+	
+		pygame.display.update()
+		
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARE_SIZE))
 			# print(event.pos)
 			
-			if turn == 0:
-				x_pos = event.pos[0]
-				col = int(math.floor(x_pos/SQUARE_SIZE))
-
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, 1)
-
+			x_pos = event.pos[0]
+			user_move = int(math.floor(x_pos / SQUARE_SIZE))
+			
+			if is_valid_location(board, user_move):
+				row = get_next_open_row(board, user_move)
+				drop_piece(board, row, user_move, 1)
+			
 				if winning_move(board, 1):
-					label = myfont.render("Player 1 Wins!!", 1, RED)
+					label = my_font.render("Player 1 Wins!!", 1, RED)
 					screen.blit(label, (40, 10))
 					game_over = True
+					print_board(board)
+					draw_board(screen, board)
+					break
 					
-			else:
-				x_pos = event.pos[0]
-				col = int(math.floor(x_pos/SQUARE_SIZE))
-
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, -1)
-
-					if winning_move(board, -1):
-						label = myfont.render("Player 2 Wins!!", 1, RED)
-						screen.blit(label, (40, 10))
-						game_over = True
-
+						
+			
+			bot_move = bot.get_next_move(board)
+			print(bot_move)
+			
+			if is_valid_location(board, bot_move):
+				row = get_next_open_row(board, bot_move)
+				drop_piece(board, row, bot_move, -1)
+				
+				if winning_move(board, -1):
+					label = my_font.render("Bot Wins!!", 1, RED)
+					screen.blit(label, (40, 10))
+					game_over = True
+					print_board(board)
+					draw_board(screen, board)
+					break
+			
 			print_board(board)
-			draw_board(board)
-
+			draw_board(screen, board)
+			
 			turn += 1
 			turn = turn % 2
 			
-			if game_over:
-				pygame.time.wait(3000)
-
-
-#%%
+	if game_over:
+		pygame.time.wait(3000)
