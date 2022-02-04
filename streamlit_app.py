8604e2242a562720ea5b160f3b8ab5aa78156bbd
Cris Torres <cristobal_javier@hotmail.com>
2022-02-04 01:02:04 -0300

    Working game mechanic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,93 @@
 import streamlit as st
 import numpy as np
+import random
 
 
-def board_init(post_init=False):
-	# if not post_init:
-	# 	st.session_state.win = {"X": 0, "O": 0}
-
-	# st.session_state.board = np.full((6, 7), ".", dtype=str)
-	
-	st.session_state.board = np.zeros((6, 7))
+def board_init():
+	st.session_state.board = np.full((6, 7), ".", dtype=str)
 	st.session_state.player = "X"
-
+	st.session_state.winner = None
 	
-def player_play(i, j):
-	st.session_state.board[i, j] = 1
-	
-def bot_play():
-	i = np.random.randint(0, 5, 1)
-	j = np.random.randint(0, 6, 1)
-	st.session_state.board[i, j] = -1
+def get_next_open_row(j):
+	for i in range(5, -1, -1):
+		if st.session_state.board[i, j] == ".":
+			return i
 
+def is_valid_location(i, j):
+	return st.session_state.board[i, j] == "."
+
+def computer_player():
+	availables_col = []
+	for j in range(7):
+		if get_next_open_row(j) != None:
+			availables_col.append(j)
+	col = random.choice(availables_col)
+	drop_piece(col)
+
+def drop_piece(j):
+	i = get_next_open_row(j)
+	if i != None:
+		st.session_state.board[i, j] = st.session_state.player
+		if not winning_move(st.session_state.player):
+			st.session_state.player = "O" if st.session_state.player == "X" else "X"
+		else:
+			st.session_state.winner = st.session_state.player
+
+def get_matrix():
+	played_mat = np.zeros((6, 7))
+	
+	for i in range(6):
+		for j in range(7):
+			if st.session_state.board[i, j] == "X":
+				played_mat[i, j] = 1
+			elif st.session_state.board[i, j] == "O":
+				played_mat[i, j] = -1
+			else:
+				played_mat[i, j] = 0
+
+	return played_mat
+
+def winning_move(piece):
+	# Check horizontal
+	for i in range(6):
+		for j in range(4):
+			if st.session_state.board[i, j] == piece and \
+				st.session_state.board[i, j+1] == piece and \
+				st.session_state.board[i, j+2] == piece and \
+				st.session_state.board[i, j+3] == piece:
+				return True
+	
+	# Check vertical
+	for i in range(3):
+		for j in range(7):
+			if st.session_state.board[i, j] == piece and \
+				st.session_state.board[i+1, j] == piece and \
+				st.session_state.board[i+2, j] == piece and \
+				st.session_state.board[i+3, j] == piece:
+				return True
+	
+	# Check positive slope diagonal
+	for i in range(3):
+		for j in range(4):
+			if st.session_state.board[i, j] == piece and \
+				st.session_state.board[i+1, j+1] == piece and \
+				st.session_state.board[i+2, j+2] == piece and \
+				st.session_state.board[i+3, j+3] == piece:
+				return True
+	
+	# Check negative slope diagonal
+	for i in range(3, 6):
+		for j in range(4):
+			if st.session_state.board[i, j] == piece and \
+				st.session_state.board[i-1, j+1] == piece and \
+				st.session_state.board[i-2, j+1] == piece and \
+				st.session_state.board[i-3, j+3] == piece:
+				return True
+				
 def main():
 	st.write(
 		"""
-		# Connect 4
+		# Connect 4 Game
 		"""
 	)
 	
@@ -31,18 +95,21 @@ def main():
 		board_init()
 	
 	for i, row in enumerate(st.session_state.board):
-		cols = st.columns([5, 1, 1, 1, 1, 1, 1, 5])
-		
+		cols = st.columns([5, 1, 1, 1, 1, 1, 1, 1, 5])
 		for j, position in enumerate(row):
-			if position > 0:
-				cols[j + 1].button("O", key=f"{i}-{j}")
-			elif position < 0:
-				cols[j + 1].button("X", key=f"{i}-{j}")
-			else:
-				cols[j + 1].button(".", key=f"{i}-{j}", on_click=player_play, args=(i, j))
-			
-	bot_play()
+			cols[j+1].button(
+				position,
+				key=f"{i}-{j}",
+				on_click=drop_piece if st.session_state.player == "X" else computer_player(),
+				args=(j, ),
+			)
+	
+	if st.session_state.winner:
+		st.write(f"{st.session_state.player} wins")
+	
+	# played_mat = get_matrix()
+	# print(played_mat)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
