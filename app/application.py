62f648b51f42c75d61e3038a0d6d2ffb74506b63
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2022-06-05 15:20:40 -0400

    Heroku settings

--- app/application.py
+++ app/application.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, request, jsonify
 from .game_bot import GameBot
 
+
 bp = Blueprint("application", __name__, url_prefix="/")
 
 @bp.route("/", methods=["GET"])
 def home():
-    global game_bot
-    game_bot = GameBot()
-
     return """Home"""
 
 @bp.route("/next", methods=["GET"])
 def get_next_move():
     board = request.args.get("board")
-    next_move = bot.get_next_move(board)
+
+    game_bot = GameBot()
+
+    next_move = game_bot.get_next_move(board)
 
     return jsonify({"next-move": next_move})
